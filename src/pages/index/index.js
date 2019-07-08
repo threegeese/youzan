a3e00214081d868d586af1e6cfe2f6eb392db636
threegeese <19970913qjh@gmail.com>
2019-07-08 19:33:24 +0800

    vue start

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,18 +1,54 @@
 import Vue from 'vue'
 import Axios from 'axios'
+import { InfiniteScroll } from 'mint-ui'
+
 import url from 'js/api.js'
+import Footer from 'components/Footer.vue'
 
 import 'css/common.css'
 import './index.css'
 
+Vue.use(InfiniteScroll)
+
 new Vue({
+
   data: {
-    lists: null
+    lists: null,
+    pageNum: 0,
+    pageSize: 6,
+    loading: false,
+    allLoaded: false
   },
+
+  components: {
+    Foot: Footer
+  },
+
   created () {
-    Axios.post(url.hotlists)
-      .then(res => {
-        this.lists = res.data.lists
+    this.getMoreHotGoods()
+  },
+
+  methods: {
+    getMoreHotGoods () {
+      if (this.allLoaded) return 
+      this.loading = true
+      Axios.post(url.hotlists, {
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }).then(res => {
+        this.pageNum += 1
+        this.loading = false
+        let currentLists = res.data.lists
+        if (currentLists.length < this.pageSize) {
+          this.allLoaded = true
+        }
+        if (this.lists) {
+          this.lists = this.lists.concat(currentLists)
+        } else {
+          this.lists = currentLists
+        }
       })
+    }
   }
+
 }).$mount('#app')
