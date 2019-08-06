2754b876cc6f5a962915130b30d9264adc0db7b4
threegeese <19970913qjh@gmail.com>
2019-08-06 15:50:23 +0800

    cart page :bug:

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,2 +1,17 @@
-import './member.css'
-import './member_base.css'
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+let routes = [{
+  path: '/',
+  components: require('./components/member.vue')
+}]
+
+let router = new Router({
+  routes
+})
+
+new Vue({
+  router
+}).$mount('#member')
